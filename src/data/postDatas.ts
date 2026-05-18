a561bf290f252b24223c027ee5e8a72f8bd4d623
ConcreteHead <dby2732010560@163.com>
2026-05-18 11:56:59 +0800

    v1 fix bugs

--- src/data/postDatas.ts
+++ src/data/postDatas.ts
@@ -1,5 +1,5 @@
 export const postImages = [
-  { src: "/public/post.jpeg", alt: "post1" },
-  { src: "/public/post.jpeg", alt: "post3" },
-  { src: "/public/ZK.ico", alt: "post2" },
+  { src: "/post.jpeg", alt: "post1" },
+  { src: "/post.jpeg", alt: "post3" },
+  { src: "/ZK.ico", alt: "post2" },
 ];
