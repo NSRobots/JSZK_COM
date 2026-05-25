a3d9921a7d9708ce7b0e5419f4470b7a4fff257f
ConcreteHead <dby2732010560@163.com>
2026-05-25 18:28:45 +0800

    add download choices

--- src/data/btnDatas.ts
+++ src/data/btnDatas.ts
@@ -8,9 +8,9 @@ export const commonItems: Record<string, ButtonItem> = {
     isRedirect: false,
   },
   download: {
-    href: "https://lcndei2tj60n.feishu.cn/docx/CoBUdVqAkoSOE4xzGxocgG8mnkd",
+    href: "/download",
     label: "下载",
-    isRedirect: true,
+    isRedirect: false,
   },
   video: {
     href: "https://lcndei2tj60n.feishu.cn/docx/TGCJdUHeKozpbexOdg3c4tGQn8d",
@@ -40,7 +40,6 @@ export const cubeBtns: CubeBtn[] = [
     ...commonItems.download,
     itemColor: "var(--white)",
     bgColor: "var(--primary-light)",
-    isRedirect: true,
     iconSvg: `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"> 
       <path d="M12 3 L12 16" /> 
       <path d="M8 13 L12 17 L16 13" /> 
@@ -51,35 +50,30 @@ export const cubeBtns: CubeBtn[] = [
     ...commonItems.video,
     itemColor: "var(--white)",
     bgColor: "var(--primary-light)",
-    isRedirect: true,
     iconSvg: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="3" y="3" width="18" height="13" rx="1" fill="currentColor" stroke="currentColor" stroke-width="1.2" stroke-linejoin="round"></rect><rect x="4.5" y="4.5" width="15" height="10" rx="0.5" fill="none" stroke="currentColor" stroke-width="1"></rect> <rect x="9" y="17" width="6" height="1.5" rx="0.5" fill="currentColor"></rect> <rect x="11" y="16" width="2" height="2" fill="currentColor"></rect></svg>`,
   },
   {
     ...commonItems.manual,
     itemColor: "var(--white)",
     bgColor: "var(--primary-light)",
-    isRedirect: true,
     iconSvg: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width="128" height="128" style="background-color: var(--primary); border-radius: 8px;"><path d="M3 3H15L21 9V21H3V3Z" stroke="var(--white)" stroke-width="2.2" stroke-linejoin="round"/><path d="M15 3V9H21L15 3Z" fill="var(--white)"/><rect x="6" y="11" width="12" height="2" rx="0.5" fill="var(--white)"/><rect x="6" y="16" width="12" height="2" rx="0.5" fill="var(--white)"/></svg>`,
   },
   {
     ...commonItems.ai,
     itemColor: "var(--white)",
     bgColor: "var(--primary-light)",
-    isRedirect: true,
     iconSvg: `<p style="font-size: 25px; font-weight: bold;">Agent</p>`,
   },
   {
     ...commonItems.feedback,
     itemColor: "var(--white)",
     bgColor: "var(--primary-light)",
-    isRedirect: true,
     iconSvg: `<p style="font-size: 25px; font-weight: bold;">Q&A</p>`,
   },
   {
     ...commonItems.sponsor,
     itemColor: "var(--white)",
     bgColor: "var(--primary-light)",
-    isRedirect: false,
     iconSvg: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="10"/><path d="M12 2L14.5 9.5H22L16 14.5L18.5 22L12 17.5L5.5 22L8 14.5L2 9.5H9.5L12 2Z" fill="white"/></svg>`,
   },
   {
@@ -89,5 +83,37 @@ export const cubeBtns: CubeBtn[] = [
     bgColor: "var(--primary-light)",
     isRedirect: false,
     iconSvg: `<p style="font-size: 25px; font-weight: bold;"><...></p>`,
+  },
+];
+
+// 下载渠道
+export const dlChannels: CubeBtn[] = [
+  {
+    label: "线路一",
+    href: "https://gitcode.com/czhaozm/czzm/releases/download/jszk/JSZK.zip",
+    iconSvg: `<p style="font-size: 25px; font-weight: bold;">.ZIP</p>`,
+    itemColor: "var(--white)",
+    bgColor: "var(--primary-light)",
+    isRedirect: true,
+  },
+  {
+    label: "微软商店",
+    href: "https://apps.microsoft.com/detail/9p7ll3d9lf7b?hl=zh-CN&gl=CN",
+    iconSvg: `<svg width="200" height="200" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"> 
+  <rect x="5" y="5" width="40" height="40" fill="var(--white)"/> 
+  <rect x="55" y="5" width="40" height="40" fill="var(--white)"/> 
+  <rect x="5" y="55" width="40" height="40" fill="var(--white)"/> 
+  <rect x="55" y="55" width="40" height="40" fill="var(--white)"/></svg>`,
+    itemColor: "var(--white)",
+    bgColor: "var(--primary-light)",
+    isRedirect: true,
+  },
+  {
+    label: "备用路线",
+    href: "https://lcndei2tj60n.feishu.cn/docx/CoBUdVqAkoSOE4xzGxocgG8mnkd",
+    iconSvg: `<p style="font-size: 25px; font-weight: bold;">DOC</p>`,
+    itemColor: "var(--white)",
+    bgColor: "var(--primary-light)",
+    isRedirect: true,
   },
 ];
