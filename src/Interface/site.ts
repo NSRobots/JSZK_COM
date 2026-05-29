396b353da29dd86fb96e89a98bda1cef93ab1b7d
ConcreteHead <dby2732010560@163.com>
2026-05-29 13:36:32 +0800

    fixed modal 4 WeChat style

--- src/Interface/site.ts
+++ src/Interface/site.ts
@@ -2,7 +2,7 @@ export interface ButtonItem {
   href: string;
   label: string;
   isRedirect?: boolean; // 是否重定向
-  isModal: boolean; // 是否触发弹窗
+  isModal?: boolean; // 是否触发弹窗
 }
 
 export interface CubeBtn extends ButtonItem {
@@ -12,9 +12,11 @@ export interface CubeBtn extends ButtonItem {
 }
 
 export interface CommonBtn extends ButtonItem {
-  height: number;
-  width: number;
-  iconSvg: string;
-  itemColor: string;
-  bgColor: string;
+  height?: number;
+  width?: number;
+  iconSvg?: string;
+  itemColor?: string;
+  bgColor?: string;
+  hoverColor?: string;
+  details?: string;
 }
